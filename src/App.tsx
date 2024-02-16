fb0e04d9012a544b433401512787e0b888b761b9
guserega <guserega.dev@gmail.com>
2024-02-16 09:43:37 +0300

    initialize react ts app; install dnd-kit & tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-3xl font-bold text-sky-500">Hello, world!</h1>
+    </div>
+  );
+};
+
+export default App;
